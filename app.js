fd13875a9cd26dbd1c87bcdda59459d4e694bdfd
João Bosco Santos <40505660+js75819@users.noreply.github.com>
2019-12-09 21:16:09 -0300

    Conclusão exercicio node modules

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 const module1 = require('./mod1')
-const module2 = require('./mod2')
+const {mensagem} = require('./mod2')
 
 module1('Teste')
-module2.mensagem('teste modulo 2')
+mensagem('teste modulo 2')
