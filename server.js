af4efc44ea3ef3f66478f17a7c1cc5ddd02d46eb
João Bosco Santos <40505660+js75819@users.noreply.github.com>
2019-12-10 20:52:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http')
 
-const requisicao = function(request, responde){
-    Response.writeHead(200), {'content.type':'text/html'}
-    Response.write('<h1>Texto a ser exibido no Browser</h1>')
-    Response.end()
+const requisicao = function(_, response){ //função underline representando request que não está sendo ultilizado.
+    response.writeHead(200), {'content.type':'text/html'}
+    response.write('<h1>Texto a ser exibido no Browser</h1>')
+    response.end()
 }
 
 const server = http.createServer(requisicao)
