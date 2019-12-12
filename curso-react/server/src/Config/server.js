408610760768874655527cf6d101208020e33407
João Bosco Santos <40505660+js75819@users.noreply.github.com>
2019-12-11 22:33:15 -0300

    Api Finalizada
    Api Finalizada e já salvando no banco de dados com o postman

--- curso-react/server/src/Config/server.js
+++ curso-react/server/src/Config/server.js
@@ -2,11 +2,13 @@ const port = 3200
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const cors = require('./cors')
 
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended:true}))
 server.use(bodyParser.json())
+server.use(cors)
 
 server.listen(port, function(){
     console.log(`Servidor no ar na porta ${port}`)
