8b51267812c0d9c8ee771c294fba680683d7e019
João Bosco Santos <40505660+js75819@users.noreply.github.com>
2019-12-13 21:47:16 -0300

    já com bootstrap e navbar

--- curso-react/site/src/App.js
+++ curso-react/site/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import {CursoIndex} from './curso'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import '../node_modules/jquery/dist/jquery.min'
+import '../node_modules/popper.js/dist/umd/popper.min'
+import '../node_modules/bootstrap/dist/js/bootstrap.min'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { CursoIndex } from './pages/curso'
+import { ContatoIndex } from './pages/contato'
+import {Menu} from './pages/menu'
 
 function App() {
   return (
-    <CursoIndex/>
+    <div>
+      <Menu/>
+      <CursoIndex />
+      <ContatoIndex />
+    </div>
+
   )
 }
 
--- curso-react/site/src/pages/curso/index.js
+++ curso-react/site/src/pages/curso/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export class CursoIndex extends React.Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>Classe curso</h1>
             </div>
         )
