6ae80f12fb21ed688081a64819fabab9c12a144d
João Bosco Santos <40505660+js75819@users.noreply.github.com>
2019-12-16 21:39:50 -0300

    RETORNANDO Banco de dados

--- curso-react/site/src/paginas/curso/form.js
+++ curso-react/site/src/paginas/curso/form.js
@@ -1,68 +1,69 @@
 import React, { Component } from 'react';
 
+
 export class FormCurso extends Component {
     render() {
         return (
             <div className="border-right pl-3 pr-3">
-            <h3 className="border-bottom">Formulário</h3>
-            <form>
-            <div className="form-group row">
-            <label for="codigo"
-            className="col-sm-3 col-form-label">
-            Código:
+                <h3 className="border-bottom">formulário</h3>
+                <form>
+                    <div className="htmlform-group row">
+                        <label htmlFor="codigo"
+                            className="col-sm-3 col-htmlform-label">
+                            Código:
             </label>
-            <div className="col-sm-5 col-6">
-            <input type="number"
-            className="form-control" id="codigo" />
-            </div>
-            </div>
-            <div className="form-group row">
-            <label for="descrição"
-            className="col-sm-3 col-form-label">
-            Descrição:
+                        <div className="col-sm-9 col-9">
+                            <input type="number"
+                                className="form-control" id="codigo" />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="descrição"
+                            className="col-sm-3 col-htmlform-label">
+                            Descrição:
             </label>
-            <div className="col-sm-9">
-            <input type="text"
-            className="form-control" id="descricao" />
-            </div>
-            </div>
-            <div className="form-group row">
-            <label for="cargaHoraria"
-            className="col-sm-3 col-form-label">
-            Carga Horária:</label>
-            <div className="col-sm-5 col-6">
-            <input type="number"
-            className="form-control" id="cargaHoraria" />
-            </div>
-            </div>
-            <div className="form-group row">
-            <label for="preco"
-            className="col-sm-3 col-form-label">
-            Preço:</label>
-            <div className="col-sm-5 col-6">
-            <input type="text"
-            className="form-control" id="preco" />
-            </div>
-            </div>
-            <div className="form-group row">
-            <label for="categoria"
-            className="col-sm-3 col-form-label">Categoria:</label>
-            <div className="col-sm-6 col-6">
-            <select className="form-control" id="categoria" >
-            <option>INFORMATICA</option>
-            <option>ENGENHARIA</option>
-            <option>ADMINISTRACAO</option>
-            <option>REDES</option>
-            </select>
-            </div>
-            </div>
-            <div className="form-group row">
-            <button
-            className="btn btn-primary ml-3 mb-3">
-            Adicionar
+                        <div className="col-sm-9">
+                            <input type="text"
+                                className="form-control" id="descricao" />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="cargaHoraria"
+                            className="col-sm-3 col-form-label">
+                            Carga Horária:</label>
+                        <div className="col-sm-9 col-9">
+                            <input type="number"
+                                className="form-control" id="cargaHoraria" />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="preco"
+                            className="col-sm-3 col-form-label">
+                            Preço:</label>
+                        <div className="col-sm-9 col-9">
+                            <input type="text"
+                                className="form-control" id="preco" />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="categoria"
+                            className="col-sm-3 col-form-label">Categoria:</label>
+                        <div className="col-sm-9 col-9">
+                            <select className="form-control" id="categoria" >
+                                <option>INFORMATICA</option>
+                                <option>ENGENHARIA</option>
+                                <option>ADMINISTRACAO</option>
+                                <option>REDES</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <button
+                            className="btn btn-primary ml-9 mb-9">
+                            Adicionar
             </button>
-            </div>
-            </form>
+                    </div>
+                </form>
             </div>
         )
     }
